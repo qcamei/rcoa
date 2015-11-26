ceb0b049a2f55274da63568f11b07c3c64b3320a
Administrator <Administrator@PC201510221817>
2015-11-26 15:43:03 +0800

    更改数据库地址

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=172.16.163.110;dbname=rcoa',
+            'dsn' => 'mysql:host=172.16.163.111;dbname=rcoa',
             'username' => 'wskeee',
             'password' => '1234',
             'charset' => 'utf8',
