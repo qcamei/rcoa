682743bc98e9c04655ceecbc267b1aa58ea56cbd
Administrator <Administrator@vioc.eecn.cn>
2016-04-05 17:04:06 +0800

    【摄影】 一、修改environments pro配置

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,20 +4,12 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=172.16.146.83;dbname=ccoa',
-            'username' => 'heyangchao',
-            'password' => '789987',
+            'dsn' => 'mysql:host=172.16.131.180;dbname=ccoa',
+            'username' => 'ccoa',
+            'password' => 'ccoa0405',
             'charset' => 'utf8',
             'tablePrefix' => 'ccoa_'   //加入前缀名称fc_
         ],
-        /*'rmsdb' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=172.16.163.111;dbname=rms',  //数据库连接&名称
-            'username' => 'wskeee',     //用户名
-            'password' => '1234',       //用户密码
-            'charset' => 'utf8',        //数据库字符类型
-            'tablePrefix' => 'rms_'   //加入前缀名称fc_
-        ],*/
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
