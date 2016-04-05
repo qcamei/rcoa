1aedfddcb36f26961435a2b3b1a05c0a80e4b4a5
Administrator <Administrator@vioc.eecn.cn>
2016-04-05 17:55:36 +0800

    【摄影】 一、删除FILEDATA_PATH 的指定。

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -8,4 +8,4 @@
 
     Yii::setAlias('filedata', dirname(dirname(__DIR__)) . '/frontend/web/filedata');
 
-    defined('FILEDATA_PATH') or define('FILEDATA_PATH', YII_DEBUG ? 'http://ccoa.tt.gzedu.net' : 'http://ccoa.gzedu.net');
+    defined('FILEDATA_PATH') or define('FILEDATA_PATH','http://ccoa.gzedu.net');
