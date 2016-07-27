87fbf1042249a73daff983ea064b24fb892129c0
wskeee <361733529@qq.com>
2016-07-27 18:21:25 +0800

    一、项目管理 1、统计添加多个条件过滤，分别为行业、层次、专业

--- frontend/modules/teamwork/controllers/StatisticsController.php
+++ frontend/modules/teamwork/controllers/StatisticsController.php
@@ -5,97 +5,168 @@ namespace frontend\modules\teamwork\controllers;
 use common\models\team\Team;
 use common\models\teamwork\CourseManage;
 use common\models\teamwork\ItemManage;
-use frontend\modules\teamwork\TeamworkTool;
+use wskeee\framework\FrameworkManager;
 use wskeee\framework\models\Item;
 use wskeee\framework\models\ItemType;
 use Yii;
 use yii\db\Query;
 use yii\helpers\ArrayHelper;
 use yii\web\Controller;
+use yii\web\Request;
 
 class StatisticsController extends Controller
 {
     public function actionIndex()
     {
-        $post = Yii::$app->getRequest()->getQueryParams();
-        $dateRange = isset($post['dateRange']) ? $post['dateRange'] : null;
-        $team = isset($post['team']) ? $post['team'] : null;
-        $status = isset($post['status'])? $post['status'] : null;
-        /** 按类型分类 */
-        $itemTyps = (new Query())
-                    ->select(['ItemType.name',
-                            'SUM(Course.lession_time) AS value'])
-                    ->from(['Course'=>CourseManage::tableName()])
-                    ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
-                    ->leftJoin(['ItemType'=>  ItemType::tableName()],'Item.item_type_id = ItemType.id')
-                    ->andFilterWhere(['Course.status'=>$status])
-                    ->andFilterWhere(['Course.`team_id`'=>$team])
-                    ->groupBy('Item.item_type_id');
-         /** 按项目分类 */
-        $items = (new Query())
-                    ->select(['FwItem.name',
-                            'SUM(Course.lession_time) AS value'])
-                    ->from(['Course'=>CourseManage::tableName()])
-                    ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
-                    ->leftJoin(['FwItem'=> Item::tableName()],'Item.item_id = FwItem.id')
-                     ->andFilterWhere(['Course.status'=>$status])
-                    ->andFilterWhere(['Course.`team_id`'=>$team])
-                    ->groupBy('Item.item_id');
-        /** 按子项目分类 */
-        $itemChilds = (new Query())
-                    ->select(['FwItem.name',
-                            'SUM(Course.lession_time) AS value'])
-                    ->from(['Course'=>CourseManage::tableName()])
-                    ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
-                    ->leftJoin(['FwItem'=> Item::tableName()],'Item.item_child_id = FwItem.id')
-                    ->andFilterWhere(['Course.status'=>$status])
-                    ->andFilterWhere(['Course.`team_id`'=>$team])
-                    ->groupBy('Item.item_child_id');
-        /** 按团队分类 */
-        $teams = (new Query())
-                    ->select(['Team.name',
-                            'SUM(Course.lession_time) AS value'])
-                    ->from(['Team'=> Team::tableName()])
-                    ->leftJoin(['Course'=>CourseManage::tableName()],'Course.team_id = Team.id')
-                    ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
-                    ->andFilterWhere(['Course.status'=>$status])
-                    ->andFilterWhere(['Course.`team_id`'=>$team])
-                    ->groupBy('Team.id');
-        if($dateRange && $status!=ItemManage::STATUS_NORMAL)
-        {
-            $dateRange_Arr = explode(" - ",$dateRange);
-            $date1 = $dateRange_Arr[0];
-            $date2 = $dateRange_Arr[1];
-
-            $itemTyps   ->andFilterWhere(['between','Course.real_carry_out',$date1,$date2]);
-            /** 按项目分类 */
-            $items      ->andFilterWhere(['between','Course.real_carry_out',$date1,$date2]);
-            /** 按子项目分类 */
-            $itemChilds ->andFilterWhere(['between','Course.real_carry_out',$date1,$date2]);
-            /** 按团队分类 */
-            $teams      ->andFilterWhere(['between','Course.real_carry_out',$date1,$date2]);
-        }
+        /* @var $request Request */
+        $request = Yii::$app->getRequest();
+        /** 行业 */
+        $item_type_id = $request->getQueryParam('item_type_id');
+        /** 层次/类型 */
+        $item_id = $request->getQueryParam('item_id');
+        /** 专业/工种 */
+        $item_child_id = $request->getQueryParam('item_child_id');
+        /** 状态 */
+        $status = $request->getQueryParam('status');
+        /** 团队 */
+        $team = $request->getQueryParam('team');
         
+        /* @var $query Query */
+        $query = (new Query())
+                ->andFilterWhere(['Course.status'=>$status])
+                ->andFilterWhere(['Course.`team_id`'=>$team])
+                ->andFilterWhere(['Item.`item_type_id`'=>$item_type_id])
+                ->andFilterWhere(['Item.`item_id`'=>$item_id])
+                ->andFilterWhere(['Item.`item_child_id`'=>$item_child_id]);
+                
+        /** 
+         * 状态为【完成】，查找【完成时间】在【划定的时间】内
+         * 状态为【在线】，查找【计划开始时间】在【划定时间】内
+         * 如果状态为空，查找【完成时间】和【计划开始时间】都在【划定时间】内
+         **/
+        if($dateRange = $request->getQueryParam('dateRange')){
+            $dateRange_Arr = explode(" - ",$dateRange);
+            if($status == null || $status == ItemManage::STATUS_CARRY_OUT)
+                $query->andFilterWhere(['between','Course.real_carry_out',$dateRange_Arr[0],$dateRange_Arr[1]]);
+            if($status == null || $status == ItemManage::STATUS_NORMAL)
+                $query->andFilterWhere(['between','Course.plan_start_time',$dateRange_Arr[0],$dateRange_Arr[1]]);
+        }
         $model = new ItemManage();
-        /* @var $twTool TeamworkTool */
-        $twTool = Yii::$app->get('twTool');
+        $teams = $this->getStatisticsByTeam($query);
+        /** 总学时 */
+        $allCHours = array_sum(ArrayHelper::getColumn($teams, 'value'));
+        return $this->render('index',[
+            'dateRange'=>$dateRange,
+            'item_type_id'=>$item_type_id,
+            'item_id'=>$item_id,
+            'item_child_id'=>$item_child_id,
+            'team'=>$team,
+            'status'=>$status,
+            'model'=>$model,
+            'allCHours'=>$allCHours,
+            
+            'twTool'=>Yii::$app->get('twTool'),
+            'itemTypes'=>$this->getStatisticsByItemType($query),
+            'items'=>$this->getStatisticsByItem($query),
+            'itemChilds'=>$this->getStatisticsByItemChild($query),
+            'teams'=>$teams,
+            'item_type_ids'=>$this->getItemTyps(),
+            'item_ids'=>$this->getItems(),
+            'teamIds'=>$this->getTeamIds(),
+        ]);
+    }
+    
+    //--------------------------------------------------------------------------
+    //
+    // utils
+    //
+    //--------------------------------------------------------------------------
+    /**
+     * 获取 行业
+     * @return Array [id=>name]
+     */
+    private function getItemTyps(){
+        $itemTyps = ItemType::find()->all();
+        return ArrayHelper::map($itemTyps, 'id', 'name');
+    }
+    /**
+     * 获取 层次/类型
+     * @return Array [id=>name]
+     */
+    private function getItems(){
+        /* @var $fwManager FrameworkManager */
+        $fwManager = Yii::$app->get('fwManager');
+        $items = $fwManager->getColleges();
+        return ArrayHelper::map($items, 'id', 'name');
+    }
+    
+    /**
+     * 获取团队ids
+     * @return Array [id=>name]
+     */
+    private function getTeamIds(){
         $teamIds = Team::find()
                     ->select(['id','name'])
                     ->asArray()
                     ->all();
         $teamIds = ArrayHelper::map($teamIds, 'id', 'name');
-        
-        return $this->render('index',[
-            'dateRange'=>$dateRange,
-            'team'=>$team,
-            'status'=>$status,
-            'teamIds'=>$teamIds,
-            'model'=>$model,
-            'twTool'=>$twTool,
-            'itemTypes'=>$itemTyps->all(Yii::$app->db),
-            'items'=>$items->all(Yii::$app->db),
-            'itemChilds'=>$itemChilds->all(Yii::$app->db),
-            'teams'=>$teams->all(Yii::$app->db)
-        ]);
+        return $teamIds;
+    }
+    /**
+     * 按行业统计
+     * @param Query $sourceQuery
+     * @return Array
+     */
+    private function getStatisticsByItemType($sourceQuery){
+        $itemTypQuery = clone $sourceQuery;
+        $itemTypQuery->select(['ItemType.name','SUM(Course.lession_time) AS value'])
+                    ->from(['Course'=>CourseManage::tableName()])
+                    ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
+                    ->leftJoin(['ItemType'=>  ItemType::tableName()],'Item.item_type_id = ItemType.id')
+                    ->groupBy('Item.item_type_id');
+        return $itemTypQuery->all(Yii::$app->db);
+    }
+    /**
+     * 按项目统计
+     * @param Query $sourceQuery
+     * @return Array
+     */
+    private function getStatisticsByItem($sourceQuery){
+        $itemQuery = clone $sourceQuery;
+        $itemQuery->select(['FwItem.name','SUM(Course.lession_time) AS value'])
+                ->from(['Course'=>CourseManage::tableName()])
+                ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
+                ->leftJoin(['FwItem'=> Item::tableName()],'Item.item_id = FwItem.id')
+                ->groupBy('Item.item_id');
+        return $itemQuery->all(Yii::$app->db);
+    }
+     /**
+     * 按子项目统计
+     * @param Query $sourceQuery
+     * @return Array
+     */
+    private function getStatisticsByItemChild($sourceQuery){
+        $itemChildQuery = clone $sourceQuery;
+        $itemChildQuery->select(['FwItem.name','SUM(Course.lession_time) AS value'])
+                    ->from(['Course'=>CourseManage::tableName()])
+                    ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
+                    ->leftJoin(['FwItem'=> Item::tableName()],'Item.item_child_id = FwItem.id')
+                    ->groupBy('Item.item_child_id');
+        return $itemChildQuery->all(Yii::$app->db);
+    }
+    
+    /**
+     * 按团队统计
+     * @param Query $sourceQuery
+     * @return Array
+     */
+    private function getStatisticsByTeam($sourceQuery){
+        $teamQuery = clone $sourceQuery;
+        $teamQuery->select(['Team.name','SUM(Course.lession_time) AS value'])
+                ->from(['Team'=> Team::tableName()])
+                ->leftJoin(['Course'=>CourseManage::tableName()],'Course.team_id = Team.id')
+                ->leftJoin(['Item'=>ItemManage::tableName()], 'Course.project_id = Item.id')
+                ->groupBy('Team.id');
+        return $teamQuery->all(Yii::$app->db);
     }
 }
--- frontend/modules/teamwork/views/statistics/index.php
+++ frontend/modules/teamwork/views/statistics/index.php
@@ -29,11 +29,73 @@ $this->params['breadcrumbs'][] = $this->title;
                     'convertFormat'=>true,
                     'pluginOptions'=>[
                         'locale'=>['format' => 'Y-m-d'],
+                        'allowClear' => true,
                     ]
                 ]);
             ?>
           </div>
         </div>
+        
+        
+        <div class="form-group">
+          <label for="item_type_id" class="col-sm-2 control-label"><?php echo Yii::t('rcoa/teamwork', 'Item Type') ?></label>
+          <div class="col-sm-10">
+                <?php 
+                echo Select2::widget([
+                    'value'=> $item_type_id,
+                    'name' => 'item_type_id',
+                    'data' => $item_type_ids,
+                    'options' => [
+                        'placeholder' => Yii::t('rcoa/teamwork', 'Statistics-Team-prompt'),
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);?>
+          </div>
+        </div>
+        
+        
+        <div class="form-group">
+          <label for="item_id" class="col-sm-2 control-label"><?php echo Yii::t('rcoa/teamwork', 'Item') ?></label>
+          <div class="col-sm-10">
+                <?php 
+                echo Select2::widget([
+                    'value'=> $item_id,
+                    'name' => 'item_id',
+                    'id' => 'item_id',
+                    'data' => $item_ids,
+                    'options' => [
+                        'placeholder' => Yii::t('rcoa/teamwork', 'Statistics-Team-prompt'),
+                        'onchange'=>'wx_one(this)',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);?>
+          </div>
+        </div>
+        
+        
+        <div class="form-group">
+          <label for="item_child_id" class="col-sm-2 control-label"><?php echo Yii::t('rcoa/teamwork', 'Item Child') ?></label>
+          <div class="col-sm-10">
+                <?php 
+                echo Select2::widget([
+                    'name' => 'item_child_id',
+                    'id' => 'item_child_id',
+                    'value'=>$item_child_id,
+                    'options' => [
+                        'placeholder' => Yii::t('rcoa/teamwork', 'Statistics-Team-prompt'),
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);?>
+          </div>
+        </div>
+        
+        
         <div class="form-group">
           <label for="team" class="col-sm-2 control-label"><?php echo Yii::t('rcoa/teamwork', 'Statistics-Team') ?></label>
           <div class="col-sm-10">
@@ -76,6 +138,8 @@ $this->params['breadcrumbs'][] = $this->title;
     </form>
     <hr/> 
     <div>
+        <div><h3><font><b>统计总学时：<?= $allCHours ?> 学时</b></font></h3></div>
+        <br/>
         <div id="itemTypeCanvas" class="chart"></div>
         <div id="itemsCanvas" class="chart"></div>
         <div id="itemChildCanvas" class="chart"></div>
@@ -83,6 +147,28 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
     
 </div>
+<script type="text/javascript">
+    function wx_one(e,select){
+	$("#item_child_id").html("");
+	$("#select2-item_child_id-container").html("全部");
+	$.post("/framework/api/search?id="+$(e).val(),function(data)
+        {
+            var selectedName = "";
+            $('<option/>').appendTo($("#item_child_id"));
+            $.each(data['data'],function()
+            {
+                $('<option>').val(this['id']).text(this['name']).appendTo($("#item_child_id"));
+                if(select && select == this['id'])
+                    selectedName = this['name'];
+            });
+            if(select)
+            {
+                $("#item_child_id").val(select);
+                $("#select2-item_child_id-container").html(selectedName);
+            }
+	});
+    }
+</script>
 
 <?= $this->render('../default/_footer',[
     'model' => $model,
@@ -98,6 +184,12 @@ $js = <<<JS
         var itemChart = new teamwork.PicChart("按项目统计",document.getElementById('itemsCanvas'),$items);
         var itemChildChart = new teamwork.PicChart("按子项目统计",document.getElementById('itemChildCanvas'),$itemChilds);
         var teamChart = new teamwork.BarChart("按团队统计",document.getElementById('teamCanvas'),$teams);
+        
+        var item_id = Number("$item_id") == "" ? -1 : Number("$item_id");
+        if(item_id!=-1)
+        {
+            wx_one($('#item_id'),Number("$item_child_id"));
+        }
 JS;
     $this->registerJs($js, View::POS_READY);
     TwStatisticsAsset::register($this);
