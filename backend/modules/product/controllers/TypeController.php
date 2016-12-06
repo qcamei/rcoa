5bd59c37a649ceb5672ae16333778ee6ab2536ba
Kiwi° <Kiwi@Kiwi>
2016-12-06 16:36:31 +0800

    一、产品（后台） 1、创建课程产品模块和模型

--- backend/modules/product/controllers/TypeController.php
+++ backend/modules/product/controllers/TypeController.php
@@ -131,7 +131,7 @@ class TypeController extends Controller
         if (($model = ProductType::findOne($id)) !== null) {
             return $model;
         } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
+            throw new NotFoundHttpException(\Yii::t('rcoa', 'The requested page does not exist.'));
         }
     }
 }
